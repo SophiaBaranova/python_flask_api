4abf233175e45ce417958b666c3a01ea9f633f17
Sophia Baranova <sonya.brnv@gmail.com>
2025-07-08 19:55:38 +0300

    Add event schedule display

--- rp_flask_api/app.py
+++ rp_flask_api/app.py
@@ -1,7 +1,7 @@
-from asyncio import events
 from flask import render_template
 import config
 from models import Event
+from events import read_one
 
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yml")
@@ -11,6 +11,12 @@ app.add_api(config.basedir / "swagger.yml")
 def home():
     events = Event.query.all()
     return render_template("home.html", events=events)
+
+
+@app.route("/schedule/<int:event_id>")
+def schedule(event_id):
+    event = Event.query.get_or_404(event_id)
+    return render_template("schedule.html", event=event)
 
 
 if __name__ == "__main__":
